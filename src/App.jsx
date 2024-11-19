c0e1edeebd42dc30a06c1d28fe3d833a74318f6a
seoa0 <seoayoung12@naver.com>
2024-11-19 13:11:50 +0900

    Add form

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import PostsLists from "./components/PostsLists";
+
 function App() {
-  return <h1>Hello World!</h1>;
+  // jsx에서 커스텀 컴포넌트 이름은 반드시 대문자로 시작
+  return (
+    // 항상 최상위 컴포넌트 단 하나로 감싸주어야 함!!
+    <PostsLists />
+  );
 }
 
 export default App;
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,8 +1,11 @@
-function Post() {
+import styles from "./Post.module.css";
+
+function Post(props) {
   return (
-    <div>
-      <p>Maximilian</p>
-      <p>React.js is awesome!</p>
+    <div className={styles.post}>
+      {/* 중괄호를 활용해 값을 동적으로 출력하기 */}
+      <p className={styles.author}>{props.author}</p>
+      <p className={styles.text}>{props.body}</p>
     </div>
   );
 }
